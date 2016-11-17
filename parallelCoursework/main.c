5857c3d12dbe2dbb135528190504adf2d595b81d
Stefan Totev <Stefan Totev>
2016-11-17 19:43:22 +0000

    Update sequential version to match the parallel one and add comments

--- parallelCoursework/main.c
+++ parallelCoursework/main.c
@@ -4,19 +4,32 @@
 #include <stdbool.h>
 #include <math.h>
 
-//Create a structure containing the elements of the matrix that can be computed in parallel
+//Contains the coordinates of the elements that can be calculated in parallel
 struct jobQueue {
     int row;
     int column;
     struct jobQueue *nextJob;
 };
-struct jobQueue *firstQueueHead = NULL;
-struct jobQueue *secondQueueHead = NULL;
-struct jobQueue *initialFirstQueueHead = NULL;
-struct jobQueue *initialSecondQueueHead = NULL;
 
-int waitingThreadsCount = 0, matrixRelaxedFlag = 0, elementsBelowPrecision = 0, size=6, iterationCounter=0;
-double *mainMatrix, *calculatedMatrix, precision = 0.002;
+//Defines the inputs that are passed to the program at run time
+struct inputs {
+    int size;
+    int numberOfThreads;
+    double precision;
+};
+
+//Defines the two queues and the initial positions of their heads
+struct jobQueue *firstQueueHead = NULL,
+                *secondQueueHead = NULL,
+                *initialFirstQueueHead = NULL,
+                *initialSecondQueueHead = NULL;
+
+//Shared variables that are needed for synchronisation of the threads
+int iterationCounter=0,
+    elementsBelowPrecision = 0;
+
+double *matrix;
+
 
 struct jobQueue* getAndForwardFirstHead() {
 
@@ -30,11 +43,12 @@ struct jobQueue* getAndForwardFirstHead() {
     }
 }
 
+//Returns head from first queue and moves it to the next element in the queue
 struct jobQueue* getAndForwardSecondHead() {
 
-    //save reference to first link
+    //Reference to the queue head that will be returned
     struct jobQueue *temporaryJob = secondQueueHead;
-    //mark next to first link as first
+    //Move head forward in the queue and return reference previous to head
     if(secondQueueHead == NULL) return NULL;
     else {
         secondQueueHead = secondQueueHead->nextJob;
@@ -55,27 +69,31 @@ void printList(struct jobQueue* head) {
    printf(" ]");
 }
 
+/* Iterates over the inner square of numbers in the matrix and
+ * adds them to either the first or the second queue.
+ */
 void insertElement(int queueNumber, int row, int column) {
-    //create a link
-    struct jobQueue *link = (struct jobQueue*) malloc(sizeof(struct jobQueue));
+    //create a new job
+    struct jobQueue *newJob = (struct jobQueue*) malloc(sizeof(struct jobQueue));
 
-    link->row = row;
-    link->column = column;
+    //Assign passed arguments to values in the new job
+    newJob->row = row;
+    newJob->column = column;
 
-    //point it to old first node
+    //Depending on the queueNumber, assigns the new job to the appropriate queue
     if (queueNumber == 1){
-        link->nextJob = firstQueueHead;
-        firstQueueHead = link;
+        newJob->nextJob = firstQueueHead;
+        firstQueueHead = newJob;
     } else if (queueNumber == 2) {
-        link->nextJob = secondQueueHead;
-        secondQueueHead = link;
+        newJob->nextJob = secondQueueHead;
+        secondQueueHead = newJob;
     }
 }
 
-void populateJobQueues() {
+void populateJobQueues(int size) {
 
     int i, j;
-
+    //Depending on the position of the element, assign it to one of the queues
     for(i = size - 2; i > 0; i--) {
         for(j = size - 2; j > 0; j--) {
             if((i+j)%2 == 0) {
@@ -85,105 +103,131 @@ void populateJobQueues() {
             }
         }
     }
-    initialFirstQueueHead = firstQueueHead;
-    initialSecondQueueHead = secondQueueHead;
 }
 
-void populateMatrix() {
-
-    FILE *matrix = fopen("arrayOfNumbers.txt", "r");
+void populateMatrix(int size) {
+    //Read from file
+    FILE *array = fopen("arrayOfNumbers.txt", "r");
 
     int i=0;
-    float number;
-
-    while(fscanf(matrix, "%f", &number) > 0 && i < size*size) {
-        mainMatrix[i] = number;
-        calculatedMatrix[i] = mainMatrix[i];
-        i++;
-    }
-
-    fclose(matrix);
-}
-
-void updateMatrix() {
-    int i, j;
-    for(i = 0; i < size; i++) {
-        for(j = 0; j < size; j++) {
-            mainMatrix[size * i + j] = calculatedMatrix[size * i + j];
+    float num;
+    //Assign as many elements to the matrix as its size
+    for(i = 0; i < size * size; i++)
+    {
+        //Assign element from the file to the matrix
+        if (fscanf(array, "%f", &num) > 0) {
+            matrix[i] = num;
+        } else {
+            //If no more elements are left in the file, exit with an error
+            printf("Not enough elements in the file!");
+            exit(1);
         }
     }
+    fclose(array);
 }
 
-void printMatrix() {
-    int i, j;
+//Adds the four numbers surrounding the element identified by "row and "column"
+void addElement(int row, int column, int size, double precision){
 
-    for (i = 0; i < size; i++) {
-        for(j = 0; j < size; j++) {
-            printf("%f ", mainMatrix[size * i + j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
+    double tempValue = matrix[size * row + column], difference;
+    //Calculate element
+    matrix[size * row + column] =
+            (matrix[size * row + (column - 1)] +
+             matrix[size * row + (column + 1)] +
+             matrix[size * (row - 1) + column] +
+             matrix[size * (row + 1) + column]) / 4;
 
-void addElement(int row, int column){
+    //Calculate the difference between the calculated element and its old value
+    difference=fabs(matrix[size*row+column]-tempValue);
 
-    calculatedMatrix[size*row+column] = (calculatedMatrix[size*row + (column - 1)] +
-                                      calculatedMatrix[size*row + (column + 1)] +
-                                      calculatedMatrix[size*(row - 1) + column] +
-                                      calculatedMatrix[size*(row + 1) + column]) / 4;
-    if (fabs(mainMatrix[size * row + column] - calculatedMatrix[size * row + column]) < precision) {
+    //If that difference is below the precision, increment "elementsBelowPrecision"
+    if (difference < precision) {
         elementsBelowPrecision++;
     }
 }
 
-void calculateJobs() {
-    int innerElementsInMatrix = (size-2)*(size-2), queueNumber = 1, elementsAreAbovePrecision = 1, i, j;
-    struct jobQueue *head;
+void calculateJobs(void* attr) {
+    //Receives the size, precision and number of threads
+    struct inputs *fileInputs = (struct inputs*)attr;
+
+    int innerElementsInMatrix = (fileInputs->size-2)*(fileInputs->size-2),
+        queueNumber = 1, //Indicates which of the two queues is being used
+        elementsAreAbovePrecision = 1; //Indicates when the loop should stop
+    struct jobQueue *head; //Serves as a copy of the current head
+
+    //Iterate over the job queue until all of the elements are under the precision
     while(elementsAreAbovePrecision) {
 
-        if(queueNumber == 1) head = getAndForwardFirstHead();
-        else head = getAndForwardSecondHead();
+        //Get the head of the linked list depending on which queue is being used
+        if (queueNumber == 1) {
+            head = getAndForwardFirstHead();
+        } else {
+            head = getAndForwardSecondHead();
+        }
 
+        //Add the next element in the queue unless all of them are calculated
         if(head != NULL) {
-            addElement(head->row, head->column);
-        } else if(head == NULL){
-            if (queueNumber == 1) {
-                firstQueueHead = initialFirstQueueHead;
-                queueNumber = 2;
-            } else {
+            addElement(head->row,
+                       head->column,
+                       fileInputs->size,
+                       fileInputs->precision);
+        }
+        //If all the elements in the queue are calculated, wait for all threads
+        else if(head == NULL){
+            //If all of the elements in the inner matrix have been calculated
+            if (queueNumber == 2)  {
+                iterationCounter++;
+                //Reset the counter for the number of elements that are above
+                //below the precision
+                if (elementsBelowPrecision < innerElementsInMatrix) {
+                    elementsBelowPrecision = 0;
+                }
                 secondQueueHead = initialSecondQueueHead;
                 queueNumber = 1;
-                iterationCounter++;
-                if (elementsBelowPrecision < innerElementsInMatrix) elementsBelowPrecision = 0;
-                updateMatrix();
-                printMatrix();
             }
-            if (elementsBelowPrecision == innerElementsInMatrix) elementsAreAbovePrecision = 0;
+            //If the queues have finished executing the first queue, they
+            //go to the second one and vice versa
+            else if (queueNumber == 1) {
+                firstQueueHead = initialFirstQueueHead;
+                queueNumber = 2;
+            }
+            //If all elements in the matrix are below the precision, break the loop
+            if (elementsBelowPrecision == innerElementsInMatrix) {
+                elementsAreAbovePrecision = 0;
+            }
         }
     }
 }
 
 int main(int argc, char *argv[])
 {
+    int size=12, numberOfThreads = 5;
+    double precision = 0.002;
 
-    mainMatrix = malloc(size * size * sizeof(double));
-    calculatedMatrix = malloc(size * size * sizeof(double));
+    matrix = malloc(size * size * sizeof(double));
+    struct inputs *fileInputs = malloc(sizeof(struct inputs));
 
-    if (mainMatrix == NULL || calculatedMatrix == NULL) {
+    fileInputs->size = size;
+    fileInputs->numberOfThreads = numberOfThreads;
+    fileInputs->precision = precision;
+
+    //Check if memory allocation was successful
+    if (matrix == NULL || fileInputs == NULL) {
         printf("Memory allocation failed!");
         return 1;
     }
 
-    populateMatrix();
-    populateJobQueues();
-    printMatrix();
+    populateMatrix(size);
+    populateJobQueues(size);
 
-    calculateJobs();
+    //Remember the original posit   ion of the heads
+    initialFirstQueueHead = firstQueueHead;
+    initialSecondQueueHead = secondQueueHead;
+
+    calculateJobs(fileInputs);
     printf("%d", iterationCounter);
 
-    //free memory from jobQueue
-    free(mainMatrix);
-    free(calculatedMatrix);
+    //Free allocated memory and destroy mutexes
+    free(matrix);
     return 0;
 }
